ed000a471f63788fd1a1adc73a56b8312cc44ad1
Ahmed-Serag19 <ahmedmohamed.amin@hotmail.com>
2025-03-17 08:35:12 +0200

    added basic navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,27 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Tabs } from "expo-router";
 
 const _layout = () => {
   return (
-    <View>
-      <Text>_layout</Text>
-    </View>
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{ title: "Home", headerShown: false }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{ title: "Search", headerShown: false }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{ title: "Saved", headerShown: false }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{ title: "Profile", headerShown: false }}
+      />
+    </Tabs>
   );
 };
 
 export default _layout;
-
-const styles = StyleSheet.create({});
